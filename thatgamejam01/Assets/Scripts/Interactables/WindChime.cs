6b7338206130337ed866fb61eebc16927949eede
NNNNateLiu <72626807+NNNNateLiu@users.noreply.github.com>
2025-12-31 12:12:54 +0800

    Add windchime interact function
    - interact in the dark, will change its mat to not hidden by fog
    - use the same interact system as the one for saving hand

---
+++ thatgamejam01/Assets/Scripts/Interactables/WindChime.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindChime : MonoBehaviour, IInteractable
+{
+    private bool _isInteracting = false;
+    public Material ignoreFogMaterial;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void OnInteract()
+    {
+        Debug.Log("player touch wind chime");
+
+        //交互后风铃一直显示
+        gameObject.GetComponent<MeshRenderer>().material = ignoreFogMaterial;
+        
+        //关闭当前smart audio
+        gameObject.GetComponent<SmartAudioSource>().enabled = false;
+        if (_isInteracting) return;
+        _isInteracting = true;
+    }
+}
