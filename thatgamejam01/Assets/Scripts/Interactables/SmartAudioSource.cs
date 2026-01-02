7c34af9e63032f900dacd7a2b2f5bdbe7e1f83ed
haodong.liao <haodong.liao@gmail.com>
2026-01-02 23:21:57 +0800

    Update audio frequency parameters for SmartAudioSource
    Increased frequency values for occluded and non-occluded states in SmartAudioSource.cs and updated corresponding values in the 01-room.unity scene. This change aims to improve audio clarity and consistency for directional and occluded sound sources.

--- thatgamejam01/Assets/Scripts/Interactables/SmartAudioSource.cs
+++ thatgamejam01/Assets/Scripts/Interactables/SmartAudioSource.cs
@@ -15,9 +15,9 @@ public class SmartAudioSource : MonoBehaviour
     [Range(0f, 1f)] public float occludedVolume = 0.4f; 
     
     [Tooltip("有遮挡 + 正对声源时的频率 (比如 1600Hz)")]
-    public float occludedFacingFreq = 3200;
+    public float occludedFacingFreq = 6400;
     [Tooltip("有遮挡 + 背对声源时的频率 (比如 600Hz)")]
-    public float occludedBackingFreq = 600f;
+    public float occludedBackingFreq = 3200;
 
     public float smoothSpeed = 10f;
 
@@ -25,10 +25,10 @@ public class SmartAudioSource : MonoBehaviour
     public bool useDirectivity = true;
     
     [Tooltip("无遮挡 + 正对声源时的频率 (清晰 22000Hz)")]
-    public float facingFreq = 22000f;
+    public float facingFreq = 25600;
     
     [Tooltip("无遮挡 + 背对声源时的频率 (闷 800Hz)")]
-    public float backingFreq = 800f;
+    public float backingFreq = 12800;
 
     [Space(10)]
     [Range(0f, 1f)] 
